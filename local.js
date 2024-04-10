7a2fe8c32dea11a979ff986fb65ae32250ea42cf
Julia Bays <jfbays2015@gmail.com>
2024-04-10 14:33:53 -0400

    rough draft of proposal

--- local.js
+++ local.js
@@ -24,3 +24,14 @@ var server = http.listen(port, () => {
     console.log(`App listening on port ${server.address().port}`);
 });
 
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
