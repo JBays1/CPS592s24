d39707d7c72a1fe1a4e1c0979679e807009e0087
Julia Bays <jfbays2015@gmail.com>
2024-04-10 19:40:38 -0400

    fix the image size

--- local.js
+++ local.js
@@ -25,35 +25,4 @@ var server = http.listen(port, () => {
 });
 
 
-// Accessibility Buttons
-$("#fontUp").click(function() {
-  pSize = parseInt($('.paragraph').css('font-size'));
-  pSize = pSize + 6;
-$('.paragraph').css('font-size', pSize);
-  cSize = parseInt($('.caption').css('font-size'));
-  cSize = cSize + 6;
-  $('.caption').css('font-size', cSize);
-});
-
-$("#fontDown").click(function() {
-  pSize = parseInt($('.paragraph').css('font-size'));
-  pSize = pSize - 6;
-$('.paragraph').css('font-size', pSize);
-  cSize = parseInt($('.caption').css('font-size'));
-  cSize = cSize - 6;
-  $('.caption').css('font-size', cSize);
-});
-
-$("#imageUp").click(function() {
-  img = parseInt($('img').css('width'));
-  img += 10;
-  $('img').css('width', img);
-});
-
-$("#imageDown").click(function() {
-  img = parseInt($('img').css('width'));
-  img -= 10;
-  $('img').css('width', img);
-});
-
 
